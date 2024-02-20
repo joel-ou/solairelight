11af6997b4511ebdf29d8260cd9e347ae46d3d13
joel <8019893@qq.com>
2024-02-20 16:43:28 +0800

    release 1.0.3-alpha

--- spring-gateway-runner/src/main/java/io/github/joelou/solairelight/gateway/GatewayApplication.java
+++ spring-gateway-runner/src/main/java/io/github/joelou/solairelight/gateway/GatewayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Joel Ou
  */
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
 public class GatewayApplication {
 
     public static void main(String[] args) {
